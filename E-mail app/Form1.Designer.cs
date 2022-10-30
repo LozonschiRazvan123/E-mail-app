682f63c19671d89a5f59fbecda7370fd4022541c
LOZONSCHI CONSTANTIN-RAZVAN <310440501RSL211122@feaa.uaic.ro>
2022-10-30 17:48:45 +0200

    Add project files.

---
+++ E-mail app/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace E_mail_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.textSubject = new System.Windows.Forms.TextBox();
+            this.textMessage = new System.Windows.Forms.TextBox();
+            this.buttonEmail = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(80, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textEmail
+            // 
+            this.textEmail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.textEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textEmail.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textEmail.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEmail.ForeColor = System.Drawing.Color.LightGray;
+            this.textEmail.Location = new System.Drawing.Point(25, 161);
+            this.textEmail.Multiline = true;
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(202, 27);
+            this.textEmail.TabIndex = 1;
+            this.textEmail.Text = "Email";
+            this.textEmail.Enter += new System.EventHandler(this.textEmail_Enter);
+            this.textEmail.Leave += new System.EventHandler(this.textEmail_Leave);
+            // 
+            // textSubject
+            // 
+            this.textSubject.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.textSubject.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textSubject.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSubject.ForeColor = System.Drawing.Color.LightGray;
+            this.textSubject.Location = new System.Drawing.Point(25, 226);
+            this.textSubject.Multiline = true;
+            this.textSubject.Name = "textSubject";
+            this.textSubject.Size = new System.Drawing.Size(202, 27);
+            this.textSubject.TabIndex = 2;
+            this.textSubject.Text = "Subject";
+            this.textSubject.Enter += new System.EventHandler(this.textSubject_Enter);
+            this.textSubject.Leave += new System.EventHandler(this.textSubject_Leave);
+            // 
+            // textMessage
+            // 
+            this.textMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
+            this.textMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textMessage.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textMessage.ForeColor = System.Drawing.Color.LightGray;
+            this.textMessage.Location = new System.Drawing.Point(25, 295);
+            this.textMessage.Multiline = true;
+            this.textMessage.Name = "textMessage";
+            this.textMessage.Size = new System.Drawing.Size(202, 81);
+            this.textMessage.TabIndex = 3;
+            this.textMessage.Text = "Message";
+            this.textMessage.Enter += new System.EventHandler(this.textMessage_Enter);
+            this.textMessage.Leave += new System.EventHandler(this.texMessage_Leave);
+            // 
+            // buttonEmail
+            // 
+            this.buttonEmail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonEmail.Location = new System.Drawing.Point(43, 406);
+            this.buttonEmail.Name = "buttonEmail";
+            this.buttonEmail.Size = new System.Drawing.Size(164, 23);
+            this.buttonEmail.TabIndex = 4;
+            this.buttonEmail.Text = "Send Email";
+            this.buttonEmail.UseVisualStyleBackColor = true;
+            this.buttonEmail.Click += new System.EventHandler(this.buttonEmail_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(257, 462);
+            this.Controls.Add(this.buttonEmail);
+            this.Controls.Add(this.textMessage);
+            this.Controls.Add(this.textSubject);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox textSubject;
+        private System.Windows.Forms.TextBox textMessage;
+        private System.Windows.Forms.Button buttonEmail;
+    }
+}
+
---
+++ E-mail app/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+
+namespace E_mail_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textEmail_Enter(object sender, EventArgs e)
+        {
+            if(textEmail.Text == "Email")
+            {
+                textEmail.Clear();
+                textEmail.ForeColor = Color.FromArgb(83, 179, 183);
+
+            }
+        }
+
+        private void textEmail_Leave(object sender, EventArgs e)
+        {
+            if(textEmail.Text=="")
+            {
+                textEmail.ForeColor = Color.FromArgb(200, 200, 200);
+                textEmail.Text = "Email";
+            }
+        }
+
+        private void textSubject_Enter(object sender, EventArgs e)
+        {
+            if (textSubject.Text == "Subject")
+            {
+                textSubject.Clear();
+                textSubject.ForeColor = Color.FromArgb(83, 179, 183);
+            }
+        }
+
+        private void textSubject_Leave(object sender, EventArgs e)
+        {
+            if (textSubject.Text == "")
+            {
+                textSubject.ForeColor = Color.FromArgb(200, 200, 200);
+                textSubject.Text = "Subject";
+            }
+        }
+
+        private void textMessage_Enter(object sender, EventArgs e)
+        {
+            if (textMessage.Text == "Message")
+            {
+                textMessage.Clear();
+                textMessage.ForeColor = Color.FromArgb(83, 179, 183);
+            }
+        }
+
+        private void texMessage_Leave(object sender, EventArgs e)
+        {
+            if (textMessage.Text == "")
+            {
+                textMessage.ForeColor = Color.FromArgb(200, 200, 200);
+                textMessage.Text = "Message";
+            }
+        }
+
+        private void buttonEmail_Click(object sender, EventArgs e)
+        {
+            string to, from, pass, messageBody;
+            MailMessage message=new MailMessage();
+            to = textEmail.Text;
+            from = "razvanlozonschi123@gmail.com";
+            pass = "jkdvgrzeemddrhvd";
+            messageBody = textMessage.Text;
+            message.To.Add(to);
+            message.From = new MailAddress(from);
+            message.Body = "from: " + "<br>Message " + messageBody;
+            message.Subject = textSubject.Text;
+            message.IsBodyHtml = true;
+            SmtpClient smtp= new SmtpClient("smtp.gmail.com");
+            smtp.EnableSsl = true;
+            smtp.Port = 587;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Credentials=new NetworkCredential(from, pass);
+            try 
+            { 
+                smtp.Send(message);
+                DialogResult code = MessageBox.Show("Email sent successfully","Email sent", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if(code==DialogResult.OK)
+                {
+                    textEmail.Clear();
+                    textSubject.Clear();
+                    textMessage.Clear();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
